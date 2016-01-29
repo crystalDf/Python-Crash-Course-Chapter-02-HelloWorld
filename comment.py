bec8bf8801d2fcda5432ecd3b1f63b5c24f6b15a
crystalDf <chendong333@gmail.com>
2016-01-29 22:46:52 +0800

    Variables

---
+++ comment.py
@@ -0,0 +1,4 @@
+# Say hello to everyone.
+print("Hello Python people!\n")
+
+import this
---
+++ name.py
@@ -0,0 +1,25 @@
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+
+print("Hello, " + full_name.title() + "!")
+
+favorite_language = "python "
+print(favorite_language)
+print(favorite_language.rstrip())
+
+favorite_language = " python"
+print(favorite_language)
+print(favorite_language.lstrip())
+
+favorite_language = " python "
+print(favorite_language)
+print(favorite_language.strip())
+
+
